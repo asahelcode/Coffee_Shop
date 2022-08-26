797bad117193792f46aa010a5c2080edacb63504
Henry <97439022+asahelcode@users.noreply.github.com>
2022-08-26 10:17:37 +0000

    create a .env file
    Add those enviroment variable with their values in them, to facilitate little secrecy in your code
    AUTH0_DOMAIN = 'coffeeshopuda.eu.auth0.com'
    ALGORITHMS = ['RS256']
    API_AUDIENCE = 'coffee'

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_env
 import json
 from flask import request, _request_ctx_stack, abort
 from functools import wraps
@@ -5,10 +7,15 @@ from jose import jwt
 from urllib.request import urlopen
 import sys
 
+load_env()
 
-AUTH0_DOMAIN = 'coffeeshopuda.eu.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee'
+AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
+ALGORITHMS = os.getenv('ALGORITHMS')
+API_AUDIENCE = os.getenv('AUDIENCE')
+
+# AUTH0_DOMAIN = 'coffeeshopuda.eu.auth0.com'
+# ALGORITHMS = ['RS256']
+# API_AUDIENCE = 'coffee'
 
 ## AuthError Exception
 '''
